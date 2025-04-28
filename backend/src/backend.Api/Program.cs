b94060c35e0d5793de1ed9826661018e181528be
James-mensah <jamesmensahcoded@gmail.com>
2025-04-28 20:35:18 +0000

    feat[backend] add Entities and DB connection

--- backend/src/backend.Api/Program.cs
+++ backend/src/backend.Api/Program.cs
@@ -1,7 +1,9 @@
+using Microsoft.EntityFrameworkCore;
 using backend.Core.Interfaces;
 using backend.Core.Services;
-
+using backend.Infrastructure.Data;
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -10,10 +12,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
+builder.Services.Configure<AppSetting>(builder.Configuration.GetSection("AppSetting"));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddSingleton<ICustomLogger>(provider => 
     new CustomLogger("Application")
 );
+
+
 var app = builder.Build();
 
 
